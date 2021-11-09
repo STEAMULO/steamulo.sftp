6f32a02fc1c365515fcf70d3bd7da9d59945d5e6
kgalea <kgalea@steamulo.com>
2021-11-09 10:45:53 +0100

    [FIX] Lint

--- molecule/default/tests/test_default.py
+++ molecule/default/tests/test_default.py
@@ -5,15 +5,18 @@ import testinfra.utils.ansible_runner
 testinfra_hosts = testinfra.utils.ansible_runner.AnsibleRunner(
     os.environ['MOLECULE_INVENTORY_FILE']).get_hosts('all')
 
+
 def test_users(host):
     assert host.user("user1").exists
     assert 'user1' in host.user("user1").groups
     assert host.user("user2").exists
     assert 'foobar' in host.user("user2").groups
 
+
 def test_home_directories(host):
     assert host.file("/var/tmp/user2").is_directory
     assert host.file("/home/user1").is_directory
+
 
 def test_sftp_directories(host):
     assert host.file("/var/tmp/user2/test1").is_directory
@@ -24,6 +27,7 @@ def test_sftp_directories(host):
     assert host.file("/home/user1/test3").is_directory
     assert host.file("/home/user1/test4").is_directory
     assert host.file("/home/user1/test5").is_directory
+
 
 def test_directories_rights(host):
     assert host.file("/var/tmp/user2/test5").mode == 0o770
